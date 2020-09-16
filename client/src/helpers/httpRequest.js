b09e029a84bca0f25f94cd9e83d00b5bc41ae863
Ivan <vaaeking@gmail.com>
2020-09-16 16:00:56 +0300

    remove comment

--- client/src/helpers/httpRequest.js
+++ client/src/helpers/httpRequest.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 export const httpRequest = (address, method, params = {}) => {
-  // const axiosReq = axios.create({
-  //   baseURL: 'http://localhost:5000'
-  // });
-
  return axios({
     method: method,
     url: address,
